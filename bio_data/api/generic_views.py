361cb323c7465f78ba048534b2d6369483fe2705
ragunath17 <ragunath06127@gmail.com>
2026-02-13 18:41:20 +0530

    Added GenericAPIVIEW with mixins implementation

---
+++ bio_data/api/generic_views.py
@@ -0,0 +1,32 @@
+from rest_framework import mixins
+from rest_framework.generics import GenericAPIView
+from bio_data.api.serializers import BiodataSerializer
+from bio_data.models import BioData
+
+class BiodataListCreateMixinView(mixins.ListModelMixin, mixins.CreateModelMixin, GenericAPIView):
+    queryset = BioData.objects.all()
+    serializer_class = BiodataSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+    
+class BiodataRetriveUpdateDestroyMixinView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, 
+                                      mixins.DestroyModelMixin, GenericAPIView):
+    queryset = BioData.objects.all()
+    serializer_class = BiodataSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+    
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
--- bio_data/api/urls.py
+++ bio_data/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from bio_data.api.views import biodata_list_create, biodata_detail
 from bio_data.api.cbv_views import BiodataListCreateCBV, BiodataDetailCBV
+from bio_data.api.generic_views import BiodataListCreateMixinView, BiodataRetriveUpdateDestroyMixinView
 
 urlpatterns = [
     path('list/', biodata_list_create, name="biodata-list"),
@@ -9,4 +10,8 @@ urlpatterns = [
     #class based view urls
     path('cbv/list/', BiodataListCreateCBV.as_view(), name="biodata-list-cbv"),
     path('cbv/<int:pk>/', BiodataDetailCBV.as_view(), name="biodata-detail-cbv"),
+    
+    #Mixins and Generic based view urls
+    path('mixin/list/', BiodataListCreateMixinView.as_view(), name="biodata-list-mixin"),
+    path('mixin/<int:pk>/', BiodataRetriveUpdateDestroyMixinView.as_view(), name="biodata-detail-mixin"),
 ]
